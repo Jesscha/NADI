43ce1507c372bb8b84a9d3e293d33608812bd056
decisive <decisive@decisiveui-noteubug.local>
2024-10-22 23:38:04 +0900

    Change folder structure

---
+++ packages/chrome-extension/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        popup: 'index.html',
+        background: 'src/background.ts',
+        
+      },
+      output: {
+        entryFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
+  },
+})
